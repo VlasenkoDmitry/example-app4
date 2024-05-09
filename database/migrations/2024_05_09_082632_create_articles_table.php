d14a3e08cb18d60a77a4637f18a96115d353d728
DzmitryV <vlasenko.dmitry1@gmail.com>
2024-05-09 19:55:48 +0300

    refactor: changing the button arrangement in show

--- database/migrations/2024_05_09_082632_create_articles_table.php
+++ database/migrations/2024_05_09_082632_create_articles_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->unsignedBigInteger('likes')->default(0);
             $table->boolean('isPublished')->default(1);
             $table->timestamps();
+
+            $table->softDeletes();
         });
     }
 
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,30 +3,40 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <h3>{{$article->title}}</h3>
-            <p class="lead">Content: {{$article->content}}</p>
-            <p class="lead">Image: {{$article->image}}</p>
-            <p class="lead">Likes: {{$article->likes}}</p>
+            <div>
+                <h3>{{$article->title}}</h3>
+                <p class="lead">Content: {{$article->content}}</p>
+                <p class="lead">Image: {{$article->image}}</p>
+                <p class="lead">Likes: {{$article->likes}}</p>
+            </div>
+        </div>
+        <div class="row row-cols-auto">
 
-            <form action="{{route('articles.edit', $article->id)}}">
-                <button type="submit" class="btn btn-primary">
-                    Edit
-                </button>
-            </form>
+            <div class="coll">
+                <form action="{{route('articles.edit', $article->id)}}">
+                    <button type="submit" class="btn btn-primary">
+                        Edit
+                    </button>
+                </form>
+            </div>
 
-            <form action="{{route('articles.destroy', $article->id)}}" method="post">
-                @csrf
-                @method('delete')
-                <button type="submit" class="btn btn-danger">
-                    Delete
-                </button>
-            </form>
+            <div class="coll">
+                <form action="{{route('articles.destroy', $article->id)}}" method="post">
+                    @csrf
+                    @method('delete')
+                    <button type="submit" class="btn btn-danger">
+                        Delete
+                    </button>
+                </form>
+            </div>
 
-            <form action="{{route('articles.index')}}">
-                <button type="submit" class="btn btn-primary">
-                    Back
-                </button>
-            </form>
+            <div class="coll">
+                <form action="{{route('articles.index')}}">
+                    <button type="submit" class="btn btn-primary">
+                        Back
+                    </button>
+                </form>
+            </div>
 
         </div>
     </div>
