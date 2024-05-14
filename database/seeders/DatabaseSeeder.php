d9420e5cfa7d800d0ed651644f3727eac0278292
DzmitryV <vlasenko.dmitry1@gmail.com>
2024-05-14 15:14:29 +0300

    refactor: add seeder and 3 factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Article;
+use App\Models\Category;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Category::factory(10)->create();
+        Tag::factory(10)->create();
+        Article::factory(100)->create();
+
+        $articles = Article::all();
+        $tags = Tag::all();
+
+        foreach ($articles as $article) {
+            $tagsIds = $tags->random(random_int(1,5))->pluck('id');
+            $article->tags()->attach($tagsIds);
+        }
     }
 }
